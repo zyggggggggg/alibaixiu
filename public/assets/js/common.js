b6668d44844afd6fc11662bf5b7537417ad39cc0
zyggggggggg <1820784487@qq.com>
2019-08-12 09:51:53 +0800

    退出登录

---
+++ public/assets/js/common.js
@@ -0,0 +1,14 @@
+$('#logout').on('click', function() {
+    if( confirm('确定要退出吗？') ){
+      $.ajax({
+        url: '/logout',
+        type: 'post',
+        success: function(data) {
+          location.href = 'login.html'
+        },
+        error: function(){
+          alert('退出失败')
+        }
+      })
+    }
+  })
